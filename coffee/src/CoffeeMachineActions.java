2b1125e6b340a752b8f66d01f1681d6aa943d04b
Tatevik Badalyan <tatevikm.badalyan@gmail.com>
2020-07-16 15:38:45 +0400

    Changes were done in the code

--- coffee/src/CoffeeMachineActions.java
+++ coffee/src/CoffeeMachineActions.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
 public class CoffeeMachineActions {
-    private static final int espresso = 0;
-    private static final int latte = 1;
-    private static final int cappuccino = 2;
+    private static final int ESPRESSO = 0;
+    private static final int LATTE = 1;
+    private static final int CAPPUCCINO = 2;
 
-    private static int[] waterForOneCup = new int[3];
-    private static int[] milkForOneCup = new int[3];
-    private static int[] coffeeForOneCup = new int[3];
-    private static int[] moneyForOneCup = new int[3];
+    private static int[] WATER_FOR_ONE_CUP = new int[3];
+    private static int[] MILK_FOR_ONE_CUP = new int[3];
+    private static int[] COFFEE_FOR_ONE_CUP = new int[3];
+    private static int[] MONEY_FOR_ONE_CUP = new int[3];
 
     private static int amountOfWater;
     private static int amountOfMilk;
@@ -21,20 +21,20 @@ public class CoffeeMachineActions {
     // set values for water, milk, coffee beans and price for each type of coffee
     private static void setupMachine()
     {
-        waterForOneCup[espresso] = 250;
-        coffeeForOneCup[espresso] = 16;
-        milkForOneCup[espresso] = 0;
-        moneyForOneCup[espresso] = 4;
+        WATER_FOR_ONE_CUP[ESPRESSO] = 250;
+        COFFEE_FOR_ONE_CUP[ESPRESSO] = 16;
+        MILK_FOR_ONE_CUP[ESPRESSO] = 0;
+        MONEY_FOR_ONE_CUP[ESPRESSO] = 4;
 
-        waterForOneCup[latte] = 350;
-        coffeeForOneCup[latte] = 20;
-        milkForOneCup[latte] = 75;
-        moneyForOneCup[latte] = 7;
+        WATER_FOR_ONE_CUP[LATTE] = 350;
+        COFFEE_FOR_ONE_CUP[LATTE] = 20;
+        MILK_FOR_ONE_CUP[LATTE] = 75;
+        MONEY_FOR_ONE_CUP[LATTE] = 7;
 
-        waterForOneCup[cappuccino] = 200;
-        coffeeForOneCup[cappuccino] = 12;
-        milkForOneCup[cappuccino] = 100;
-        moneyForOneCup[cappuccino] = 6;
+        WATER_FOR_ONE_CUP[CAPPUCCINO] = 200;
+        COFFEE_FOR_ONE_CUP[CAPPUCCINO] = 12;
+        MILK_FOR_ONE_CUP[CAPPUCCINO] = 100;
+        MONEY_FOR_ONE_CUP[CAPPUCCINO] = 6;
 
         // machine resources at this moment
         amountOfWater = 400;
@@ -55,23 +55,23 @@ public class CoffeeMachineActions {
 
     private static void buy()
     {
-        int coffeeType = readIntegerValue("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino");
+        int coffeeType = readIntegerValue("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:");
         --coffeeType;
 
-        amountOfWater -=  waterForOneCup[coffeeType];
-        amountOfMilk -= milkForOneCup[coffeeType];
-        amountOfCoffee -= coffeeForOneCup[coffeeType];
-        amountOfMoney += moneyForOneCup[coffeeType];
+        amountOfWater -=  WATER_FOR_ONE_CUP[coffeeType];
+        amountOfMilk -= MILK_FOR_ONE_CUP[coffeeType];
+        amountOfCoffee -= COFFEE_FOR_ONE_CUP[coffeeType];
+        amountOfMoney += MONEY_FOR_ONE_CUP[coffeeType];
 
         amountOfCups--;
     }
 
     private static void fill()
    {
-        amountOfWater += readIntegerValue("Write how many ml of water do you want to add");
-        amountOfMilk += readIntegerValue("Write how many ml of milk do you want to add");
-        amountOfCoffee += readIntegerValue("Write how many grams of coffee beans do you want to add");
-        amountOfCups += readIntegerValue("Write how many disposable cups of coffee do you want to add");
+        amountOfWater += readIntegerValue("Write how many ml of water do you want to add:");
+        amountOfMilk += readIntegerValue("Write how many ml of milk do you want to add:");
+        amountOfCoffee += readIntegerValue("Write how many grams of coffee beans do you want to add:");
+        amountOfCups += readIntegerValue("Write how many disposable cups of coffee do you want to add:");
    }
 
     private static void take()
@@ -80,16 +80,17 @@ public class CoffeeMachineActions {
         amountOfMoney = 0;
     }
 
-
     private static int readIntegerValue(String msg)
     {
-        System.out.print(msg + ":\n> ");
+        System.out.println(msg);
+        System.out.print("> ");
         return scan.nextInt();
     }
 
     private static String readStringValue(String msg)
     {
-        System.out.print(msg + ":\n> ");
+        System.out.println(msg);
+        System.out.print("> ");
         return scan.nextLine();
     }
 
