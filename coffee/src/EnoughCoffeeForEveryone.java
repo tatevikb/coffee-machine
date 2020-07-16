c41e2d8fbdf894bb72918451d7346354b7ed2c62
Tatevik Badalyan <tatevikm.badalyan@gmail.com>
2020-07-16 15:21:15 +0400

    Minor change is done in the code

--- coffee/src/EnoughCoffeeForEveryone.java
+++ coffee/src/EnoughCoffeeForEveryone.java
@@ -2,34 +2,36 @@ import java.util.Scanner;
 
 public class EnoughCoffeeForEveryone {
 
-    private static final int waterForOneCup = 200;
-    private static final int milkForOneCup = 50;
-    private static final int coffeeForOneCup = 15;
+    private static final int WATER_FOR_ONE_CUP = 200;
+    private static final int MILK_FOR_ONE_CUP = 50;
+    private static final int COFFEE_FOR_ONE_CUP = 15;
 
     private static Scanner scan = new Scanner(System.in);
 
     // Prints message and reads the value from command line
     private static int readValueFor(String msg)
     {
-        System.out.print(msg + ":\n> ");
+        System.out.println(msg);
+        System.out.print("> ");
+
         return scan.nextInt();
     }
 
     // Counts how many cups you will have with the available resources of milk, water and coffee
-    private static int countAmountOfCups(int aw, int am, int ac)
+    private static int countAmountOfCups(int amountOfWater, int amountOfMilk, int amountOfCoffee)
     {
-        int w = aw / waterForOneCup;
-        int m = am / milkForOneCup;
-        int c = ac / coffeeForOneCup;
+        int w = amountOfWater / WATER_FOR_ONE_CUP;
+        int m = amountOfMilk / MILK_FOR_ONE_CUP;
+        int c = amountOfCoffee / COFFEE_FOR_ONE_CUP;
 
         return Math.min(w, Math.min(m, c));
     }
 
     /* The method checks, has the coffee machine enough supplies to make the specified amount of coffee?
     And prints the result */
-    private static String canMakeRequiredAmount(int cups, int aw, int am, int ac)
+    private static String canMakeRequiredAmount(int cups, int amountOfWater, int amountOfMilk, int amountOfCoffee)
     {
-        int canMake = countAmountOfCups(aw, am, ac);
+        int canMake = countAmountOfCups(amountOfWater, amountOfMilk, amountOfCoffee);
         int diffCups = canMake - cups;
 
         if(diffCups == 0)
